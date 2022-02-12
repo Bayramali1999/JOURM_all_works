a0974382afa94b860e4a3a25fcb47ed5af28e213
Admin <babdiyev361@gmail.com>
2022-02-12 10:43:29 +0500

    ui is readya

---
+++ app/src/main/java/com/example/jourm/ui/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.example.jourm.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jourm.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
